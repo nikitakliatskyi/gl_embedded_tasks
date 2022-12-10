640f44de29e20c7575b19c768a508696fadf3e36
nikitakliatskyi <nikitakliatskyi@gmail.com>
2022-12-10 23:54:32 +0200

    made variables from pure_c_01 point to const string literals

--- pure_c_01/hello_world.c
+++ pure_c_01/hello_world.c
@@ -2,11 +2,12 @@
 
 int main() {
   int grp_num = 1;
-  char *full_name = "Nikita Kliatskyi";
-  char *email = "nikitakliatskyi@gmail.com";
-  char *repo_link = "https://github.com/nikitakliatskyi/gl_embedded_tasks";
-  char *folder_link = "https://drive.google.com/drive/u/0/folders/"
-                      "1FLC5xR5qWh6Gvv0KEkOKksEONlrEbbE8";
+  const char *full_name = "Nikita Kliatskyi";
+  const char *email = "nikitakliatskyi@gmail.com";
+  const char *repo_link =
+      "https://github.com/nikitakliatskyi/gl_embedded_tasks";
+  const char *folder_link = "https://drive.google.com/drive/u/0/folders/"
+                            "1FLC5xR5qWh6Gvv0KEkOKksEONlrEbbE8";
   printf("Group number: %d\n"
          "Full name: %s\n"
          "Email: %s\n"
